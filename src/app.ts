3246e26778ee270d19fc4570ae7c2325bf2fd921
iamSaugata27 <84305894+iamSaugata27@users.noreply.github.com>
2021-11-04 17:39:15 +0530

    docker node

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import { config } from 'dotenv'
+
+config()
+let a = 5
+let b = 9
+console.log(`Result is ${a + b}`);
+console.log('dotenv printing...')
+console.log(process.env.language);
+console.log(process.env.dev);
+// console.log(process.env.DATABASE_URL);
