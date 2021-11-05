63f7ff6935ea9089dd306ff11e3aa44337a3fc5f
iamSaugata27 <84305894+iamSaugata27@users.noreply.github.com>
2021-11-05 16:23:55 +0530

    docker build and push added 11

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:alpine
 COPY . .
 RUN npm install
-# ARG LANGUAGE
-# ARG DEV
+ARG LANGUAGE
+ARG DEV
 ENV LANGUAGE=$LANGUAGE
 ENV DEV=$DEV
 CMD [ "npm","run","server" ]
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ config()
 let a = 5
 let b = 9
 console.log(`Result is ${a + b}`);
-console.log('dotenv printing.....')
+console.log('dotenv printing...')
 console.log(process.env.LANGUAGE);
 console.log(process.env.DEV);
 // console.log(process.env.DATABASE_URL);
