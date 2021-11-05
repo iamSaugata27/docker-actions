effb9804e30b816945c949c08a50d835a2565061
iamSaugata27 <84305894+iamSaugata27@users.noreply.github.com>
2021-11-05 11:30:05 +0530

    docker build and push added 7

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ let a = 5
 let b = 9
 console.log(`Result is ${a + b}`);
 console.log('dotenv printing.....')
-console.log(process.env.language);
-console.log(process.env.dev);
+console.log(process.env.LANGUAGE);
+console.log(process.env.DEV);
 // console.log(process.env.DATABASE_URL);
